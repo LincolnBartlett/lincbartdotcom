e87f687f0613362d83b3ee5d5b1c91b660b2db50
LincolnBartlett <linkwin@Main.localdomain>
2019-08-31 12:23:51 -0700

    added react app

---
+++ client/lincbart/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Front from './components/front.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Front />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/lincbart/src/components/front.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Front() {
+    return (
+        <div className="">
+            <p>It works</p>
+        </div>
+    );
+}
+
+export default Front; 
